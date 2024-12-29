b094b5a49293631f2a3e315de0fad7d851dfc48f
Håvard Brynjulfsen <havardob@hotmail.com>
2024-12-30 00:44:30 +0100

    adds checkHabit function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,82 @@
 'use client';
 import styles from "./page.module.css";
 import Checkbox from "@/components/checkbox";
-import React, {useEffect, useId, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import IconDots from "@/components/icons/iconDots";
 import {v4 as uuid} from "uuid";
 
-
-function scrollToEnd(element: any) {
-    element.scrollLeft = element.scrollWidth;
+type Habit = {
+    id: string,
+    name: string,
+    checkedBoxes: string[]
 }
 
 export default function Home() {
-    useEffect(() => {
-        getFromLocalStorage();
+    const [habits, setHabits] = useState<Habit[]>([]);
+
+    const dates = useMemo(() => {
+        const startDate = new Date("2024-12-01");
+        const today = new Date();
+        const result: Date[] = [];
+        const date = new Date(startDate);
+        while (date <= today) {
+            result.push(new Date(date));
+            date.setDate(date.getDate() + 1);
+        }
+        return result;
     }, []);
 
-    const startDate = new Date("2024-12-01");
-    const today = new Date();
-
-    let [habits, setHabits] = useState<{ id: string, name: string, checkedDays: string[] }[]>([]);
-
-    let dates: Date[] = [];
-
-    let date = new Date(startDate);
-    while (date <= today) {
-        dates.push(new Date(date));
-        date.setDate(date.getDate() + 1);
-    }
-
-    function newHabit() {
-        let newHabitName = prompt("New habit");
-        if (newHabitName) {
-            setHabits([{id: uuid(), name: newHabitName, checkedDays: []}, ...habits])
-            saveToLocalStorage();
-        }
-    }
-
-    function getFromLocalStorage() {
+    useEffect(() => {
         const storedHabits = localStorage.getItem('storage');
         if (storedHabits) {
             setHabits(JSON.parse(storedHabits));
         }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('storage', JSON.stringify(habits));
+    }, [habits]);
+
+
+    function newHabit() {
+        const newHabitName = prompt("New habit");
+        if (newHabitName) {
+            setHabits((habits) => [
+                {
+                    id: uuid(),
+                    name: newHabitName,
+                    checkedBoxes: []
+                },
+                ...habits
+            ]);
+        }
     }
 
-    useEffect(() => {
-        saveToLocalStorage();
-    }, [habits]);
-
-    useEffect(() => {
-        document.querySelectorAll("[data-row]").forEach((row) => {
-            scrollToEnd(row);
-        });
-    }, [habits]);
-
-    function saveToLocalStorage() {
-        localStorage.setItem('storage', JSON.stringify(habits));
+    function checkHabit(habitId: string, checkboxId: string) {
+        setHabits((habits) =>
+            habits.map((habit) =>
+                habit.id === habitId
+                    ? {
+                        ...habit,
+                        checkedBoxes: habit.checkedBoxes.includes(checkboxId)
+                            ? habit.checkedBoxes.filter(item => item !== checkboxId)
+                            : [...habit.checkedBoxes, checkboxId],
+                    }
+                    : habit
+            )
+        );
     }
 
     return (
         <div className={styles.page}>
             <header className={styles.header}>
-                <button onClick={() => saveToLocalStorage()}>About</button>
+                <button>About</button>
                 <h1>My streak</h1>
                 <button onClick={newHabit}>New +</button>
             </header>
             <main className={styles.main}>
                 {habits.map((habit, index) => (
-                    <div key={index} className={styles.habit}>
+                    <div key={habit.id + index} className={styles.habit}>
                         <div className={styles.habit__header}>
                             <h3>{habit.name}</h3>
                             <button>
@@ -74,19 +84,28 @@ export default function Home() {
                                 <IconDots />
                             </button>
                         </div>
-                        <div className={styles.row} data-row={habit.id} key={index}>
-                            {dates.map((date, index) => (
-                                <Checkbox
-                                    key={index}
-                                    id={habit.id + date.toISOString()}
-                                    label={
-                                        {
-                                            title: date.toLocaleDateString("en-GB", {weekday: "narrow"}),
-                                            subtitle: date.toLocaleDateString("en-GB", {month: "short", day: "numeric"})
+                        <div className={styles.row} data-row={habit.id} key={habit.id}>
+                            {dates.map((date) => {
+                                const checkboxId = habit.id + date.toISOString();
+                                const isChecked = habit.checkedBoxes.includes(checkboxId);
+
+                                return (
+                                    <Checkbox
+                                        key={checkboxId}
+                                        id={checkboxId}
+                                        label={
+                                            {
+                                                title: date.toLocaleDateString("en-GB", {weekday: "narrow"}),
+                                                subtitle: date.toLocaleDateString("en-GB", {
+                                                    month: "short",
+                                                    day: "numeric"
+                                                })
+                                            }
                                         }
-                                    }
-                                />
-                            ))}
+                                        isChecked={isChecked}
+                                        onClickAction={() => checkHabit(habit.id, date.toISOString())} />
+                                )
+                            })}
                         </div>
                     </div>
                 ))}
--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -5,24 +5,28 @@ import React, {useState} from "react";
 
 interface CheckboxProps {
     id: string;
-    label: {title: string, subtitle?: string};
+    label: { title: string, subtitle?: string };
+    onClickAction: () => void;
+    isChecked: boolean;
 }
 
 
-export default ({id, label}: CheckboxProps) => {
-    const [checked, setChecked] = useState(false);
+export default function Checkbox({id, label, onClickAction, isChecked}: CheckboxProps) {
+    const [checked, setChecked] = useState(isChecked);
 
     const handleChange = () => {
         setChecked(!checked);
     };
 
+
+
     return (
         <div className={styles.checkbox}>
             <label htmlFor={id}>
-            <span className={styles.title}>{label.title}</span>
-            <span className={styles.subtitle}>{label.subtitle}</span>
+                <span className={styles.title}>{label.title}</span>
+                <span className={styles.subtitle}>{label.subtitle}</span>
             </label>
-            <input id={id} type="checkbox" checked={checked} onChange={handleChange} />
+            <input id={id} type="checkbox" checked={checked} onChange={handleChange} onClick={onClickAction} />
         </div>
     )
 }
